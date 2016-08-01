ed74961830efafad045387a6babbce47c07881bb
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-08-01 10:12:01 +0700

    setup test suite

---
+++ phaser-comments-d-ts-test.ts
@@ -0,0 +1,4 @@
+/// <reference path="./phaser/typescript/phaser.comments.d.ts" />
+
+class MyGame extends Phaser.Game {
+}
---
+++ phaser-d-ts-test.ts
@@ -0,0 +1,4 @@
+/// <reference path="./phaser/typescript/phaser.d.ts" />
+
+class MyGame extends Phaser.Game {
+}
